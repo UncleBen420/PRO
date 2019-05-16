88ba1173414bb23589e58f4b744a86e352124d49
UncleBen420 <remyvu@outlook.com>
2019-05-16 21:56:45 +0200

    refactoring and debugging

--- src/main/java/JTreeManager/JTreeManager.java
+++ src/main/java/JTreeManager/JTreeManager.java
@@ -1,16 +1,16 @@
 package JTreeManager;
 
-
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.BoxLayout;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
@@ -27,7 +27,6 @@ import java.util.logging.Logger;
 import searchfilters.AbstractTreeFilter;
 import jsontreeparse.JsonTreeParser;
 import properties.PropertiesHandler;
-
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreeNode;
 
@@ -42,7 +41,7 @@ public class JTreeManager extends JPanel {
 	 */
 	private static final long serialVersionUID = 774488936584418358L;
 	private final List<AbstractTreeFilter> Filtre;
-	private final DefaultMutableTreeNode root;
+	private DefaultMutableTreeNode root;
 	private JTree tree;
 	private final File rootDirectory;
 	private final File JsonTree;
@@ -51,19 +50,19 @@ public class JTreeManager extends JPanel {
 	private Parser parserTag = new Parser();
 	private final Semaphore mutex = new Semaphore(1);
 	private JTextField messageBox;
-
+	private Boolean flagFilter = false;
 	private String value = "";
+	JPanel waitingPanel = null;
 
-    /**
-     *
-     */
-    public JTreeManager() {
-        this.Filtre = new ArrayList<>();
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.setSize(new Dimension(500,500));
-        this.setPreferredSize(new Dimension(100,100));
-        
-        
+	/**
+	 *
+	 */
+	public JTreeManager() {
+		this.Filtre = new ArrayList<>();
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setSize(new Dimension(500, 500));
+		this.setPreferredSize(new Dimension(100, 100));
+
 		Properties properties = PropertiesHandler.parseProperties();
 
 		this.rootDirectory = new File(properties.getProperty("imageBankPath"));
@@ -72,28 +71,193 @@ public class JTreeManager extends JPanel {
 		JsonTreeParser parser = new JsonTreeParser();
 
 		if (this.JsonTree.exists()) {
+
 			root = parser.setDirectoryTree(properties.getProperty("JsonBankPath"));
+			showTree();
+			flagFilter = true;
 
 		} else {
-			parser.createXML(rootDirectory);
 
-			root = parser.setDirectoryTree((new File(properties.getProperty("JsonBankPath")).getAbsolutePath()));
+			JTreeManager manager = this;
+
+			Thread thread;
+			thread = new Thread() {
+				@Override
+				public void run() {
+
+					parser.createXML(rootDirectory);
+					root = parser
+							.setDirectoryTree((new File(properties.getProperty("JsonBankPath")).getAbsolutePath()));
+					manager.showTree();
+					flagFilter = true;
+
+				}
+			};
+
+			thread.start();
+
+			showWaiting();
+
+		}
+	}
+
+	/**
+	 *
+	 * @param f
+	 */
+	public void addFiltre(final AbstractTreeFilter f) {
+
+		if (!flagFilter) {
+			return;
 		}
 
-		tree = null;
+		Thread thread;
+		thread = new Thread() {
+			@Override
+			public void run() {
+
+				try {
+					mutex.acquire();
+				} catch (InterruptedException e) {
+
+				}
+
+				setText(f.toString() + " is running.");
+
+				tree.setEnabled(false);
+
+				Filtre.add(f);
+				f.setTree(tree);
+				f.filtreTree();
+
+				tree.setEnabled(true);
+
+				releaseText();
+
+				mutex.release();
+
+			}
+		};
+
+		thread.start();
+
+	}
+
+	/**
+	 *
+	 * @param f
+	 */
+	public void removeFiltre(final AbstractTreeFilter f) {
+
+		if (!flagFilter) {
+			return;
+		}
+
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+
+				try {
+					mutex.acquire();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+
+				}
+
+				setText(f.toString() + " is deleted");
+				tree.setEnabled(false);
+
+				int i = Filtre.size();
+
+				for (i--; i >= Filtre.indexOf(f); i--) {
+
+					Filtre.get(i).unfiltreTree();
+
+				}
+
+				Filtre.remove(f);
+
+				for (i++; i < Filtre.size(); i++) {
+
+					Filtre.get(i).filtreTree();
+
+				}
+
+				tree.setEnabled(true);
+				releaseText();
+
+				mutex.release();
+
+			}
+		};
+
+		thread.start();
+
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 *
+	 * @param s
+	 */
+	public void setSlider(SliderDemo s) {
+		slider = s;
+	}
+
+	/**
+	 *
+	 * @param t
+	 */
+	public void setTable(ViewerTable t) {
+		table = t;
+	}
+
+	private void setText(String text) {
+
+		messageBox.setEditable(true);
+		messageBox.setText(text);
+		messageBox.setEditable(false);
+
+	}
+
+	private void releaseText() {
+
+		messageBox.setEditable(true);
+		messageBox.setText("");
+		messageBox.setEditable(false);
+
+	}
+
+	private void showTree() {
+
+		if (waitingPanel != null) {
+
+			this.remove(waitingPanel);
+			waitingPanel = null;
+
+		}
 
 		tree = new JTree(root);
-		
-		tree.setMinimumSize(new Dimension(100,100));
+
+		tree.setMinimumSize(new Dimension(100, 100));
 
 		tree.addMouseListener(new MouseAdapter() {
 
-                        @Override
+			@Override
 			public void mouseClicked(MouseEvent e) {
 
 				if (e.getClickCount() == 2 && tree.isEnabled()) {
 
-					if (FilenameUtils.getExtension(((DefaultMutableTreeNode) tree.getLastSelectedPathComponent()).toString()).equals("jpg")) {
+					if (FilenameUtils
+							.getExtension(((DefaultMutableTreeNode) tree.getLastSelectedPathComponent()).toString())
+							.equals("jpg")) {
 
 						value = rootDirectory.getAbsolutePath();
 
@@ -115,147 +279,36 @@ public class JTreeManager extends JPanel {
 			}
 
 		});
-		
+
 		JScrollPane sb = new JScrollPane();
 		sb.setViewportView(tree);
 		sb.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		this.add(sb);
-		
+
 		messageBox = new JTextField();
 		messageBox.setEditable(false);
 		this.add(messageBox);
-
+		this.repaint();
 	}
 
-    /**
-     *
-     * @param f
-     */
-    public void addFiltre(final AbstractTreeFilter f) {
-    	
-    	JPanel manager = this;
+	private void showWaiting() {
 
-		Thread thread;
-                thread = new Thread() {
-                @Override
-                public void run() {
-                    
-                    try {
-                        mutex.acquire();
-                    } catch (InterruptedException e) {
-                        
-                    }
-                    
-                    
-                    setText(f.toString() + " is running.");
-                    
-                    tree.setEnabled(false);
-                    JLabel label = new JLabel();
-                    manager.add(label);
-                    Filtre.add(f);
-                    f.setTree(tree);
-                    f.filtreTree();
-                    
-                    tree.setEnabled(true);
-                    
-                    releaseText();
-                    
-                    mutex.release();
-                    
-                    
-                }
-            };
+		waitingPanel = new JPanel();
 
-		thread.start();
+		waitingPanel.setLayout(new BoxLayout(waitingPanel, BoxLayout.Y_AXIS));
+
+		messageBox = new JTextField();
+		JOptionPane.showMessageDialog(null,
+				"The program cannot with the json Directory tree file,\nIt start parsing a new one please wait a few minutes");
+
+		messageBox.setEditable(false);
+		JProgressBar pb = new JProgressBar();
+		pb.setIndeterminate(true);
+
+		waitingPanel.add(messageBox, BorderLayout.SOUTH);
+		waitingPanel.add(pb, BorderLayout.SOUTH);
+
+		this.add(waitingPanel, BorderLayout.SOUTH);
 
 	}
-
-    /**
-     *
-     * @param f
-     */
-    public void removeFiltre(final AbstractTreeFilter f) {
-
-		Thread thread = new Thread() {
-                        @Override
-			public void run() {
-
-				try {
-					mutex.acquire();
-				} catch (InterruptedException e) {
-                                    // TODO Auto-generated catch block
-
-				}
-				
-				setText(f.toString() + " is deleted");
-				tree.setEnabled(false);
-
-				int i = Filtre.size();
-
-				for (i--; i >= Filtre.indexOf(f); i--) {
-
-					Filtre.get(i).unfiltreTree();
-
-				}
-
-				Filtre.remove(f);
-
-				for (i++; i < Filtre.size(); i++) {
-
-					Filtre.get(i).filtreTree();
-
-				}
-				
-				tree.setEnabled(true);
-				releaseText();
-				
-				mutex.release();
-
-			}
-		};
-
-		thread.start();
-
-	}
-
-    /**
-     *
-     * @return
-     */
-    public String getValue() {
-		return value;
-	}
-
-    /**
-     *
-     * @param s
-     */
-    public void setSlider(SliderDemo s) {
-		slider = s;
-	}
-
-    /**
-     *
-     * @param t
-     */
-    public void setTable(ViewerTable t) {
-		table = t;
-	}
-    
-    private void setText(String text) {
-
-    		messageBox.setEditable(true);
-            messageBox.setText(text);
-            messageBox.setEditable(false);
-
-    	
-    }
-    
-    private void releaseText() {
-    	
-    	messageBox.setEditable(true);
-        messageBox.setText("");
-        messageBox.setEditable(false);
-
-    }
 }
---
+++ src/main/java/exceptionHandler/LogFileWritingHandler.java
@@ -0,0 +1,36 @@
+package exceptionHandler;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+public class LogFileWritingHandler {
+
+	public static void handleException(String m, StackTraceElement[] s) {
+
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(
+					new OutputStreamWriter(new FileOutputStream(new File("ErrorLog.txt")), StandardCharsets.UTF_16));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		pw.println(m);
+		System.out.println(m);
+		
+		for(StackTraceElement ste : s) {
+			pw.println(ste.toString());
+			System.out.println(ste.toString());
+		}
+		
+
+		pw.close();
+		System.exit(1);
+	}
+
+}
